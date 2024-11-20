b4bc60d251cd9b856510591034d966644cc6bbf6
Daniel Escobar <daniel.escobar6@u.icesi.edu.co>
2024-11-20 00:11:01 -0500

    dunno

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 
 export const useLogin = () => {
     const login = async (email: string, password: string) => {
-        const authService = new AuthService("https://integrative-task-2-team.onrender.com/");
+        const authService = new AuthService("https://integrative-task-2-team.onrender.com");
         const user = await authService.login(email, password);
         if (user)
             Cookies.set("currentUser", JSON.stringify(user));
