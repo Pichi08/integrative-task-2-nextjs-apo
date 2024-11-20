e3513ee5cbc3448f9cf87f98aeda2aaabe7585a0
Daniel Escobar <daniel.escobar6@u.icesi.edu.co>
2024-11-19 23:33:31 -0500

    dependencies

--- src/hooks/register/useRegister.ts
+++ src/hooks/register/useRegister.ts
@@ -5,8 +5,8 @@ import { RegisterUserService } from "@/services/registerUser.service"
 
 export const useRegister = () => {
     const register = async (name: string, email: string, password: string) => {
-        // const registerUserService = new RegisterUserService("https://integrative-task-2-team.onrender.com/");
-        const registerUserService = new RegisterUserService("http://0.0.0.0:8000/");
+        const registerUserService = new RegisterUserService("https://integrative-task-2-team.onrender.com/");
+        // const registerUserService = new RegisterUserService("http://0.0.0.0:8000/");
 
         try {
             const registerUser = await registerUserService.registerUser(name, email, password);
